ee072345efdf1ee517ad3a9f2b48c18e993613f0
Sebastian Pięta <spieta2@gmail.com>
2021-03-31 10:32:21 +0200

    Character controller done

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,14 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float jumpForce;
+    [SerializeField] private LayerMask groundMask;
+    [SerializeField] private Transform groundCheckPosition;
+    [SerializeField] private float groundCheckRadius;
+
+    [SerializeField] private LayerMask interactableMask;
+    [SerializeField] private Transform interactionPoint;
+    [SerializeField] private float interactionRadius;
+
     [SerializeField] private float movementSpeed;
     [Range(0f, 1f)] [SerializeField] private float movementSmoothing;
 
@@ -14,10 +22,12 @@ public class PlayerController : MonoBehaviour
     private Vector2 current_velocity;
     private Rigidbody2D rigid;
     private bool playerDirection;
+    private bool isGrounded;
 
     private void Awake()
     {
-        rigid = GetComponent<Rigidbody2D>();    
+        rigid = GetComponent<Rigidbody2D>();
+        isGrounded = true;
     }
 
     public void OnMove(InputAction.CallbackContext context)
@@ -41,12 +51,14 @@ public class PlayerController : MonoBehaviour
     {
         Interact();
         Move();
+        GroundedCheck();
         Jump();
     }
 
     private void Move()
     {
         if (movementInput == 0) return;
+
         Vector2 target = new Vector2(movementInput * movementSpeed, rigid.velocity.y);
         rigid.velocity = Vector2.SmoothDamp(rigid.velocity, target, ref current_velocity, movementSmoothing);
         Flip();
@@ -56,21 +68,35 @@ public class PlayerController : MonoBehaviour
     {
         if (movementInput > 0 && !playerDirection) return;
         if (movementInput < 0 && playerDirection) return;
+
         transform.localScale = new Vector3(transform.localScale.x * -1f, transform.localScale.y, transform.localScale.z);
         playerDirection = !playerDirection;
     }
 
     private void Interact()
     {
+        if (!interactInput) return;
+        interactInput = false;
 
+        var colliders = Physics2D.OverlapCircleAll(interactionPoint.position, interactionRadius, interactableMask);
+        foreach (var col in colliders)
+            col.GetComponent<Interactable>().Interact();
+    }
+
+    private void GroundedCheck()
+    {
+        if (isGrounded) return;
+
+        var colliders = Physics2D.OverlapCircleAll(groundCheckPosition.position, groundCheckRadius, groundMask);
+        if (colliders.Length > 0) isGrounded = true;
     }
 
     private void Jump()
     {
-        if(jumpInput)
+        if(jumpInput && isGrounded)
         {
             jumpInput = false;
-            // need grounded check
+            isGrounded = false;
             rigid.AddForce(Vector2.up * jumpForce);
         }
     }
